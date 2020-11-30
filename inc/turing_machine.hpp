f6e89fa0034b0743a479e9d8b952f6e32ef4a6c8
Paulo Ricardo J. Miranda <pauloricardojm@hotmail.com.br>
2020-11-30 19:23:53 -0300

    Turing Machine description

---
+++ inc/turing_machine.hpp
@@ -0,0 +1,30 @@
+#ifndef TURING_MACHINE_HPP
+    #define TURING_MACHINE_HPP
+
+        #include <iostream>
+        #include <unordered_set>
+        #include <algorithm>
+        #include <string>
+
+        using namespace std;
+        
+        namespace TuringMachine
+        {
+            enum Command
+            {
+                L, R, S
+            };
+
+            class Machine 
+            {
+                private:
+                    size_t num_states;
+                    unsigned int actual_state, rw_head;
+                    string tape;
+                    unordered_set<string> tape_sym, alph_sym;
+                
+                public:
+                    Machine();
+            };
+        }
+#endif
