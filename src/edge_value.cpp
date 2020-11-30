fe52afdddc4a921820477e149a006f79168c843a
Paulo Ricardo J. Miranda <pauloricardojm@hotmail.com.br>
2020-11-30 19:24:12 -0300

    Implementation of EdgeValue class

---
+++ src/edge_value.cpp
@@ -0,0 +1,28 @@
+#include <inc/edge_value.hpp>
+
+EdgeValue::EdgeValue()
+{
+
+}
+
+EdgeValue::EdgeValue(string condition, string write_to, Command cmd)
+{
+    this->condition = condition;
+    this->write_to = write_to;
+    this->cmd = cmd;
+}
+
+string EdgeValue::get_condition()
+{
+    return this->condition;
+}
+
+Command EdgeValue::get_command()
+{
+    return this->cmd;
+}
+
+string EdgeValue::get_write_to()
+{
+    return this->write_to;
+}
