a209262c6f986c5b3616dac886d18ad1ebb5b6f9
Paulo Ricardo J. Miranda <pauloricardojm@hotmail.com.br>
2020-11-30 19:30:51 -0300

    Modified to work with EdgeValue class

--- inc/graph.hpp
+++ inc/graph.hpp
@@ -7,7 +7,7 @@
         #include <utility>
         #include <algorithm>
         #include <iterator>
-        #include <string>
+        #include <inc/edge_value.hpp>
 
         using namespace std;
 
@@ -15,13 +15,13 @@
         {
             private:
                 size_t node_qt;
-                vector<list<pair<unsigned int, string>>> graph;
+                vector<list<pair<unsigned int, EdgeValue>>> graph;
             
             public:
                 Graph();
                 Graph(size_t node_qt);
                 Graph(const Graph &g);
-                int add_edge(unsigned int i_node, unsigned int j_node, string value);
+                int add_edge(unsigned int i_node, unsigned int j_node, EdgeValue value);
                 vector<int> edge_pair(unsigned int i_node, string value);
                 void print_all_edges();
                 size_t nodes_qt();
