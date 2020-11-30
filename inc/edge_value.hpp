8a40a4f3d2d28f8f0cb87f79b3c28b634fcd7e3e
Paulo Ricardo J. Miranda <pauloricardojm@hotmail.com.br>
2020-11-30 19:50:23 -0300

    Added some comments

--- inc/edge_value.hpp
+++ inc/edge_value.hpp
@@ -3,6 +3,14 @@
         #include <inc/turing_machine.hpp>
 
         using namespace TuringMachine;
+        
+
+        /*
+            In a turing machine, an transition is made of 3 objects: an condition,
+            an valid tape symbol to write to in the position which read/write heads points, and 
+            a command to the read/write head (move to the left (L), to the right (R) or stays (S)).
+            The purpose of this class is to describe an valid transition for TuringMachine class.
+        */
 
         class EdgeValue
         {
@@ -11,11 +19,13 @@
                 Command cmd;
             
             public:                
-                EdgeValue();
-                EdgeValue(string, string, Command);
-                string get_condition();
-                Command get_command();
-                string get_write_to();
+                EdgeValue(); // Empty constructor
+                EdgeValue(string, string, Command); // Constructor with condition, symbol to write and the command for the head
+                string get_condition(); // Returns the condition of the transition
+                Command get_command(); // Returns the command of the transition
+                string get_write_to(); // Returns the symbol to write 
+                
+                /* Overloading the output stream */
                 friend ostream &operator<<(ostream &out, const EdgeValue &e)
                 {
                     const string cmd_text[3] = { "L", "R", "S"};
