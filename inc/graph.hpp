835105f298fd7e0c51eb828dd80c765a60608f13
Paulo Ricardo J. Miranda <pauloricardojm@hotmail.com.br>
2020-11-30 19:48:28 -0300

    Added some comments

--- inc/graph.hpp
+++ inc/graph.hpp
@@ -11,6 +11,11 @@
 
         using namespace std;
 
+        /*
+            This class serves to represents the graph of an deterministic Turing Machine.
+            It is implemented in a adjacency list way, in which their nodes is a pair.
+        */
+
         class Graph
         {
             private:
@@ -18,11 +23,11 @@
                 vector<list<pair<unsigned int, EdgeValue>>> graph;
             
             public:
-                Graph();
-                Graph(size_t node_qt);
-                Graph(const Graph &g);
-                int add_edge(unsigned int i_node, unsigned int j_node, EdgeValue value);
-                vector<int> edge_pair(unsigned int i_node, string value);
+                Graph(); // Empty constructor
+                Graph(size_t node_qt); // Construct a graph with pre-determined number of nodes "node_qt"
+                Graph(const Graph &g); // Copy constructor
+                int add_edge(unsigned int i_node, unsigned int j_node, EdgeValue value); // Adds an edge in the graph
+                vector<int> edge_pair(unsigned int i_node, string value); // Returns an node identification if finds an transition with condition "value"
                 void print_all_edges();
                 size_t nodes_qt();
                 ~Graph();
