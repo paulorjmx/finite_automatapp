17032c15ae0b9eb1df6806d99315bce28e647b5f
Paulo Ricardo J. Miranda <pauloricardojm@hotmail.com.br>
2020-12-06 01:59:52 -0300

    Moved EdgeValue to Edge in this class

--- src/graph.cpp
+++ src/graph.cpp
@@ -8,7 +8,7 @@ class edge_value
 
     public:
         edge_value(string s) : value(s) { }
-        bool operator () (pair<unsigned int, EdgeValue> p) { return value == p.second.get_condition(); }
+        bool operator () (pair<unsigned int, Edge> p) { return value == p.second.condition; }
 };
 
 Graph::Graph()
@@ -24,21 +24,21 @@ Graph::Graph(const Graph &g)
 
 Graph::Graph(size_t node_qt)
 {
-    vector<list<pair<unsigned int, EdgeValue>>> tmp_graph(node_qt, list<pair<unsigned int, EdgeValue>>());
+    vector<list<pair<unsigned int, Edge>>> tmp_graph(node_qt, list<pair<unsigned int, Edge>>());
     this->node_qt = node_qt;
     this->graph = tmp_graph;
 }
 
 
 // Adds an edge with value "value" in the graph
-int Graph::add_edge(unsigned int i_node, unsigned int j_node, EdgeValue value)
+int Graph::add_edge(unsigned int i_node, unsigned int j_node, Edge value)
 {
     int ret = -1;
     if(i_node >= 0 && i_node < this->node_qt)
     {
         if(j_node >= 0 && j_node < this->node_qt)
         {
-            this->graph[i_node].push_back(pair<unsigned int, EdgeValue>(j_node, value));
+            this->graph[i_node].push_back(pair<unsigned int, Edge>(j_node, value));
             ret = 0;
         }
     }
@@ -50,12 +50,13 @@ int Graph::add_edge(unsigned int i_node, unsigned int j_node, EdgeValue value)
     Searches for a node with transition value "value".
     It uses the find_if and edge_value functor for it.
 */
-pair<unsigned int, EdgeValue> Graph::edge_pair(unsigned int i_node, string value)
+pair<unsigned int, Edge> Graph::edge_pair(unsigned int i_node, string value)
 {
-    pair<unsigned int, EdgeValue> j_node;
-    list<pair<unsigned int, EdgeValue>>::iterator it;
-    list<pair<unsigned int, EdgeValue>> tmp = this->graph[i_node];
+    pair<unsigned int, Edge> j_node;
+    list<pair<unsigned int, Edge>>::iterator it;
+    list<pair<unsigned int, Edge>> tmp = this->graph[i_node];
     it = find_if(tmp.begin(), tmp.end(), edge_value(value));
+    // cout << (*it).first << ": " << (*it).second.condition << endl;
     j_node = (*it);
     return j_node;
 }
@@ -66,7 +67,7 @@ void Graph::print_all_edges()
 {
     for(int i = 0; i < this->node_qt; i++)
     {
-        for(list<pair<unsigned int, EdgeValue>>::iterator it = this->graph[i].begin(); it != this->graph[i].end(); it++)
+        for(list<pair<unsigned int, Edge>>::iterator it = this->graph[i].begin(); it != this->graph[i].end(); it++)
         {
             cout << i << " -> " << (*it).first << ": " << (*it).second << endl;
         }
