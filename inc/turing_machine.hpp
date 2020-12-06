95a5ed3e6f38bdd3f486c94317859e3fa101a323
Paulo Ricardo J. Miranda <pauloricardojm@hotmail.com.br>
2020-12-06 02:33:43 -0300

    Added final_state, graph and constructor

--- inc/turing_machine.hpp
+++ inc/turing_machine.hpp
@@ -5,26 +5,23 @@
         #include <unordered_set>
         #include <algorithm>
         #include <string>
+        #include <inc/graph.hpp>
 
         using namespace std;
         
-        namespace TuringMachine
+        class Machine 
         {
-            enum Command
-            {
-                L, R, S
-            };
+            private:
+                size_t num_states;
+                unsigned int actual_state, rw_head, final_state;
+                string tape;
+                unordered_set<string> tape_sym, alph_sym;
+                Graph graph;
+            
+            public:
+                // Machine();
+                Machine(size_t num_states, unsigned int final_state, string tape, Graph g);
+                bool check_string(string st);
 
-            class Machine 
-            {
-                private:
-                    size_t num_states;
-                    unsigned int actual_state, rw_head;
-                    string tape;
-                    unordered_set<string> tape_sym, alph_sym;
-                
-                public:
-                    Machine();
-            };
-        }
+        };
 #endif
