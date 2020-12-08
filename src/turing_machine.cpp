cfb4b3ef6a119acace4de321dcfc8345e467ff2b
Paulo Ricardo J. Miranda <pauloricardojm@hotmail.com.br>
2020-12-08 03:10:34 -0300

    Fixed change_tape: now resets the machine and make empty tape

--- src/turing_machine.cpp
+++ src/turing_machine.cpp
@@ -6,7 +6,14 @@ Machine::Machine(unsigned int final_state, unordered_set<string> term, unordered
     this->final_state = final_state; // Acceptance state of the machine
     this->tape_sym = ext;
     this->alph_sym = term;
-    this->tape = "B" +tape+ "B"; // Tape that will be read and write by the head
+    if(tape == "-") // If its lambda
+    {
+        this->tape = "BB"; // Two white squares to indicate empty tape
+    }
+    else 
+    {
+        this->tape = "B" +tape+ "B"; // Tape that will be read and write by the head
+    }
     this->graph = g; // Graph that contains all transitions and states
     this->rw_head = 1; // Initial position of read/write head
     this->actual_state = 0; // Actual state of the machine
@@ -21,16 +28,16 @@ int Machine::run()
         j_node = this->graph.edge_pair(this->actual_state, this->tape.substr(this->rw_head, 1));
         if(j_node.second.condition != "")
         {
-            if(this->tape_sym.count(j_node.second.write_to) > 0)
-            {
+            // if(this->tape_sym.count(j_node.second.write_to) > 0)
+            // {
                 this->tape.replace(this->rw_head, 1, j_node.second.write_to);
                 this->rw_head = this->rw_head + j_node.second.cmd;
                 this->actual_state = j_node.first;
-            }
-            else 
-            {
-                break;
-            }
+            // }
+            // else 
+            // {
+                // break;
+            // }
         }
         else 
         {
@@ -44,7 +51,18 @@ int Machine::run()
 
 void Machine::change_tape(string new_tape)
 {
-    this->tape = "B" +new_tape+ "B";
+
+    /* Resets the Turing Machine */
+    this->actual_state = 0;
+    this->rw_head = 1;
+    if(new_tape == "-") // If its lambda
+    {
+        this->tape = "BB"; // Two white squares to indicate empty tape
+    } 
+    else 
+    {
+        this->tape = "B" +new_tape+ "B"; // New tape to be read/write by the machine;
+    }
 }
 
 void Machine::print_tape()
